caa15f2b9ea73569b023ce8541e65258755f93d7
Heartz4kukxie <kuukuaeshun151@gmail.com>
2025-10-13 19:32:12 +0000

    This is my code for the ZimStyle Boutique To keep up with growing demand,

---
+++ src/ClothingItem.java
@@ -0,0 +1,115 @@
+public class ClothingItem {
+    private String name;
+    private double price;
+    private Size size;
+    private ClothingType type;
+    private double discountedPrice;
+    // Creating an instance data field and adding one class variables
+    public static   int itemCount =0;
+
+    public static int getItemCount() {
+        return itemCount;
+    }
+// Creating public accessor  and mutator methods for each data fields
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Size getSize(){
+        return size;
+    }
+    public void setSize(Size size){
+        this.size =size;
+    }
+
+    public ClothingType getType(){
+        return type;
+
+    }
+    public void setType(ClothingType type){
+        this.type = type;
+    }
+
+
+
+    // Task 3
+
+    //Creating a parameterized constructor that initializes all fields
+    public ClothingItem(String name ,double price ,Size size, ClothingType type){
+        this.name = name;
+        this.price =price;
+        this.size=size;
+        this.type=type;
+        itemCount++;
+    }
+    //Creating a  partial constructor that initializes only name and type
+    public ClothingItem(String name , ClothingType type){
+        this.name = name;
+        this.type=type;
+        this.size = Size.M;   //
+        this.price = 1000;
+        itemCount++;
+    }
+    //Creating a default constructor
+    public ClothingItem(){
+        name= "Generic African Wear";
+        type = ClothingType.SHIRT;
+        size = Size.M;
+        price = 1000;
+        itemCount++;
+    }
+
+
+
+    //Task 4
+    public void displayInfo(){
+        System.out.println("Name: " + name + " | Type: " + type + " | Size: " + size + " | Price: " + price +"Zig");
+
+    }
+
+    public void displayInfo(boolean showPrice) {
+        if(showPrice){
+            System.out.println("Name: " + name + " | Type: " + type + " | Size: " + size + " | Price: " + price +"Zig");
+
+        }else{
+            System.out.println("Name: " + name + " | Type: " + type + " | Size: " + size);
+
+        }
+
+
+        }//Task 6
+    public void applyDiscount() {
+        if (price > 50) {
+            double discountedPrice = 0.9 * price;
+            double discount = price - discountedPrice;
+
+            price = discountedPrice;
+
+            System.out.println("Your discount is " + discount);
+            System.out.println("Your new price is " + price +"Zig");
+
+
+        } else {
+            System.out.println("No discount applies");
+
+
+
+        }
+
+    }
+
+}
+
---
+++ src/ZimStyleApp.java
@@ -0,0 +1,72 @@
+import java.util.Scanner;
+// Zimbabwe boutique code
+public class ZimStyleApp {
+    public static void main(String[] args) {
+        ClothingType type = ClothingType.DRESS;
+        System.out.println("Enter your clothing type:");
+        Scanner input = new Scanner(System.in);
+        String userCloth = input.next().toUpperCase();
+
+        try {
+            ClothingType selectedType = ClothingType.valueOf(userCloth);
+            System.out.println(selectedType);
+
+            //a small switch statement  that prints a description for  the clothing types.
+            switch (selectedType) {
+                case DRESS:
+                    System.out.println("Elegant wear made from traditional fabric");
+                    break;
+                case TROUSER:
+                    System.out.println("Perfect for formal and casual African wear.");
+                    break;
+            }
+
+
+        }catch (IllegalArgumentException e) {
+            System.out.println("Invalid input");
+
+
+        }
+        ClothingItem itemOne = new ClothingItem();
+        ClothingItem itemTwo = new ClothingItem( "Ankara Pants" ,100, Size.S,ClothingType.TROUSER);
+        ClothingItem itemThree = new ClothingItem("Blouse",ClothingType.SHIRT);
+
+
+
+        //Task 6
+        //Creating  a ClothingItem array
+
+        ClothingItem[] inventory = new ClothingItem [5];
+        //creating 5 different instances
+        inventory[0] = new ClothingItem();
+        inventory[1] = new ClothingItem("Ankara Pants", 150, Size.S, ClothingType.TROUSER);
+        inventory[2] = new ClothingItem("Blouse", ClothingType.SHIRT);
+        inventory[3] = new ClothingItem("African Jacket", 2500, Size.L, ClothingType.JACKET);
+        inventory[4] = new ClothingItem("Beaded Necklace", 45, Size.S, ClothingType.ACCESSORY);
+
+        for(ClothingItem item : inventory){
+            item.displayInfo();
+        }
+        System.out.println(ClothingItem.getItemCount());
+        for(ClothingItem item : inventory){
+            item.applyDiscount();
+        }
+        double total = 0;
+        for (ClothingItem item : inventory) {
+            total += item.getPrice();
+        }
+
+        double average = total / inventory.length;
+
+        //output in two decimal places
+        System.out.printf("Average Price for All Items: $%.2f%n", average);
+
+
+
+
+        input.close();
+    }
+}
+
+
+
